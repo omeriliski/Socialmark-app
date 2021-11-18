5bfbd15e08f9f26e46bcc881a38c2d504fb20d5b
Ömer Iliski <omeriliski06@gmail.com>
2021-11-18 15:06:41 +0100

    added socket client

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -3,7 +3,10 @@
         <AppHeader/>
         <div class="flex flex-row">
             <SideBar/>
-            <component :is="$route.meta.componentName" :items="bookmarkList"/>
+            <div class="socket">
+                <input type="text" @keydown.enter="SEND_MESSAGE">
+            </div>
+            <!-- <component :is="$route.meta.componentName" :items="bookmarkList"/> -->
             <!-- {{$log("route",$route)}} -->
             <!-- <AppBookmarkList v-if="bookmarkList.length>0" :items="bookmarkList"/>
             <div v-else>Bookmark bulunamadi...</div> -->
@@ -14,6 +17,7 @@
 <script>
 import AppHeader from "@/components/shared/appHeader";
 import SideBar from "@/components/Account/sideBar";
+import io from "socket.io-client";
 export default {
     components:{
         SideBar,
@@ -21,8 +25,9 @@ export default {
     },
     data() {
         return {
-            bookmarkList:[]
-        }
+            bookmarkList:[],
+            socket:{}
+        };
     },
     
     created() {
@@ -30,6 +35,18 @@ export default {
             console.log(res);
             this.bookmarkList=res.data;
         })
+    },
+    mounted() {
+        this.socket=io("http://localhost:2021");
+        this.socket.on("WELCOME_MESSAGE",this.WELCOME_MESSAGE);
+    },
+    methods: {
+        WELCOME_MESSAGE(data){
+            console.log(data);
+        },
+        SEND_MESSAGE(e){
+            this.socket.emit("MESSAGE", e.target.value);
+        }
     },
 }
 </script>
